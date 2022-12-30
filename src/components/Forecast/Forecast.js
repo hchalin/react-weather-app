e8db5a358c16693e5c0b5276a143e54b9d39c29d
hchalin <hchalin98@gmail.com>
2022-12-29 19:48:04 -0700

    made days in a week array

--- src/components/Forecast/Forecast.js
+++ src/components/Forecast/Forecast.js
@@ -6,8 +6,25 @@ import {
   AccordionItemButton,
 } from "react-accessible-accordion";
 
+const WEEK_DAYS = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
 const Forecast = ({ data }) => {
-  console.log(data)
+  // make array of the next 7 days
+  const dayInAWeek = new Date().getDay();
+
+  const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(
+    WEEK_DAYS.slice(0, dayInAWeek)
+  );
+
+  console.log(forecastDays);
+  console.log(data);
   return (
     <>
       <label className="title">Daily</label>
@@ -15,7 +32,16 @@ const Forecast = ({ data }) => {
         {data.list.splice(0, 7).map((item, idx) => (
           <AccordionItem key={idx}>
             <AccordionItemHeading>
-              <AccordionItemButton>Hello</AccordionItemButton>
+              <AccordionItemButton>
+                <div className="daily-item">
+                  <img
+                    src={`icons/${item.weather[0].icon}.png`}
+                    alt=""
+                    className="icon-small"
+                  />
+                  <label className="day"></label>
+                </div>
+              </AccordionItemButton>
             </AccordionItemHeading>
             <AccordionItemPanel></AccordionItemPanel>
           </AccordionItem>
